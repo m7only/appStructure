1b3739eb3a24c9d626af6b3d67ae9691104a1780
Only <ongranly@ngs.ru>
2023-02-04 20:17:08 +0700

    ДЗ структура веб-приложения

---
+++ src/main/java/m7/appstructure/AppStructureApplication.java
@@ -0,0 +1,13 @@
+package m7.appstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStructureApplication.class, args);
+    }
+
+}
